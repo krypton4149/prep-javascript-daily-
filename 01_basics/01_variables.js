0a3ab6f6025a2911b411e1703dbd0a022e61bd29
Piyush Yadav <73812714+krypton4149@users.noreply.github.com>
2025-07-09 18:16:02 +0000

    variable_done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 123456;
+var accountEmail = "piyush@gmail.com"
+let password = "123456789"
+accountcity = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail= "piyuh@gmail.com"
+password= "4567"
+accountcity="benglure"
+
+/*
+Prefer not to use var
+becuse of issues in bloack scope and functional scope
+
+*/
+
+console.log(accountId);
+console.table([accountEmail,password,accountcity])
+
