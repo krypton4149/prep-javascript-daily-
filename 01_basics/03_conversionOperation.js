a1c63e50501e392fb67613341fda92efe0e385b5
Piyush Yadav <73812714+krypton4149@users.noreply.github.com>
2025-07-14 16:51:07 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log (typeof ValueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// " " => false
+// "piyush" => true
