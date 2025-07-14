1b902be802405df3a5dc973f6df4689d14e4aafa
Piyush Yadav <73812714+krypton4149@users.noreply.github.com>
2025-07-14 16:30:19 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,30 @@
 "use strict"; // treat all js code as a newer version
 
-// alert("hello")   // we are using node js not broswing
+// alert("3+3")   // we are using node js not broswing
 
+console.log(3
+    +3 
+) // code readability should be high
+
+
+console.log("Piyush")
+
+let name = "Piyush"
+let age = 24
+let isLoggedIn = false
+let state ;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone Value
+// undefined 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
